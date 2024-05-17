541cf336fe838be367e958479706661d7a83aa6f
KareemElozeiri <k.elozeiri@gmail.com>
2024-05-17 21:07:42 +0300

    feat(symbol_mapper): add symbol mapper

--- symbol_mapper.m
+++ symbol_mapper.m
@@ -2,12 +2,14 @@ function [out_symbols] = symbol_mapper(bits_stream, type)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 
-    out_symbols = bits_stream;
     
     if type == "16-QAM"
-        
+        bits_per_symbol = 4;
+        symbol_indices = bi2de(reshape(bits_stream, bits_per_symbol, []).', 'left-msb');
+        qam_constellation = qammod(0:15, 16);
+        out_symbols = qam_constellation(symbol_indices + 1);
     elseif type == "BPSK"
-        
+        out_symbols = 2 * bits_stream - 1;
     end
 
 
